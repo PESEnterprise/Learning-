f103624dfac25a06e9ddac7c3d2444fcd4fd0d97
PESEnterprise <156096290+PESEnterprise@users.noreply.github.com>
2024-01-12 12:11:52 +0000

    Added my project

---
+++ even.py
@@ -0,0 +1,6 @@
+print("Knowing an even or a odd number")
+number = int(input("Enter your number: "))
+if number % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
