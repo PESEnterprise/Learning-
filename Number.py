7ee7164801e0cc7b2cbdfaa8624d572a77fb3ea2
PESEnterprise <156096290+PESEnterprise@users.noreply.github.com>
2024-01-15 20:44:02 +0000

    Added my project

--- Number.py
+++ Number.py
@@ -2,7 +2,7 @@
 #assuming we all are die at 90 years
 #then you minus your current age with the year you are dieing (90)
 #then multiply the remaining years with the weeks in a year (52)
-age = "18"
+age = "16"
 years = 90 - int(age)
 weeks = years * 52
 #f-string
